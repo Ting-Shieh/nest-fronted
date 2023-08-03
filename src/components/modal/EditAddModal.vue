c9e15c14d1ce45aadbdaeb5d9193a8f92f4d1613
Ting-Shieh <zsting29@gmail.com>
2023-08-03 16:53:03 +0800

    feat: 串接用戶列表，把模態框先抽離，之後補寫，之後補寫hook

---
+++ src/components/modal/EditAddModal.vue
@@ -0,0 +1,7 @@
+<!--
+ Copyright (c) 2023 Ting<zsting29@gmail.com>
+ 
+ This software is released under the MIT License.
+ https://opensource.org/licenses/MIT
+-->
+
--- src/views/users/index.vue
+++ src/views/users/index.vue
@@ -11,28 +11,36 @@
       <thead>
         <tr>
           <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
+          <th scope="col">用户名</th>
+          <th scope="col">角色</th>
+          <th scope="col">性别</th>
+          <th scope="col">大頭</th>
+          <th scope="col">Email</th>
+          <th scope="col">操作</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colspan="2">Larry the Bird</td>
-          <td>@twitter</td>
+        <tr v-for="(item, index) in list" :key="item.id">
+          <th scope="row">{{ index + 1 }}</th>
+          <td>{{ item.username }}</td>
+          <td>{{ item.roles?.map(o => o.name).join(',') }}</td>
+          <td>{{ item.profile?.gender }}</td>
+          <td>{{ item.profile?.photo }}</td>
+          <td>{{ item.profile?.address }}</td>
+          <td>
+            <button
+              type="button"
+              class="btn btn-secondary px-3"
+            >
+              <i class="far fa-edit me-2"></i>Edit
+            </button>
+            <button
+              type="button"
+              class="btn btn-danger px-3 ms-3"
+            >
+              <i class="far fa-trash-alt me-2"></i>Delete
+            </button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -48,7 +56,34 @@
   </div>
 </template>
 <script setup lang="ts">
-import {} from 'vue';
+import axios from '@/utils/axios';
+import { onMounted, ref } from 'vue';
+interface Profile {
+  gender: number;
+  address: string;
+  photo: string;
+}
+
+interface RoleItem {
+  id: number;
+  name: string;
+}
+
+interface UserItem {
+  id: number;
+  username: string;
+  password?: string;
+  profile: Profile;
+  roles: RoleItem[];
+}
+const list = ref([] as UserItem[])
+async function init() {
+  list.value = await axios.get('/user') as UserItem[];
+}
+
+onMounted(async () => {
+  await init();
+})
 </script>
 <style lang="scss" scoped>
 
