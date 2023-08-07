10b3ab7e06daeadbb79bccffa4409a67ad94b866
Ting-Shieh <zsting29@gmail.com>
2023-08-07 23:13:58 +0800

    feat: 角色與菜單管理頁面初始化

--- src/views/menus/index.vue
+++ src/views/menus/index.vue
@@ -6,7 +6,48 @@
 -->
 
 <template>
-  <div class="">Menus page</div>
+  <div class="mb-3">
+    <button type="button" class="btn btn-primary px-3">
+      <i class="fas fa-plus"></i>新增
+    </button>
+  </div>
+  <table class="table table-bordered table-hover table-striped">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Menu名稱</th>
+        <th scope="col">Menu路徑</th>
+        <th scope="col">排序</th>
+        <th scope="col">操作策略</th>
+        <th scope="col">操作</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>首頁</td>
+        <td>/dashboard</td>
+        <td>1</td>
+        <td>Read, Update</td>
+        <td>
+          <button type="button" class="btn btn-secondary px-3">
+            <i class="far fa-edit me-2"></i>Edit</button>
+          <button type="button" class="btn btn-danger px-3 ms-3">
+            <i class="far fa-trash-alt me-2"></i>Delete
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+      </ul>
+    </nav>
 </template>
 <script setup lang="ts">
 import {} from 'vue';
--- src/views/roles/index.vue
+++ src/views/roles/index.vue
@@ -6,7 +6,44 @@
 -->
 
 <template>
-  <div class="">Roles page</div>
+  <div class="mb-3">
+    <button type="button" class="btn btn-primary px-3">
+      <i class="fas fa-plus"></i>新增
+    </button>
+  </div>
+  <table class="table table-bordered table-hover table-striped">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">角色名稱</th>
+        <th scope="col">分配權限</th>
+        <th scope="col">操作</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>管理员</td>
+        <td></td>
+        <td>
+          <button type="button" class="btn btn-secondary px-3">
+            <i class="far fa-edit me-2"></i>Edit</button>
+          <button type="button" class="btn btn-danger px-3 ms-3">
+            <i class="far fa-trash-alt me-2"></i>Delete
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+      </ul>
+    </nav>
 </template>
 <script setup lang="ts">
 import {} from 'vue';
